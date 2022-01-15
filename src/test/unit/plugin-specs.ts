475ec7970f2f0fea6206790f4ada887c7b9caa78
Jonathan Lipps <jlipps@gmail.com>
2022-01-14 19:40:56 -0800

    chore: upgrade earljs

--- src/test/unit/plugin-specs.ts
+++ src/test/unit/plugin-specs.ts
@@ -4,6 +4,7 @@ import { expect } from 'earljs'
 
 describe('AppiumOcrPlugin', function() {
     it('should ready a worker', async function() {
+        expect(AppiumOcrPlugin).toBeDefined()
         const p = new AppiumOcrPlugin('foo')
         expect(p.isWorkerReady).toEqual(false)
         await p.readyWorker(new BaseDriver())
